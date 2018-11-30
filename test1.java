43484d57f804ea610a04513f51df8c2b7a432178
Franck <Franck@po>
2018-11-30 11:58:41 +0100

    c4

--- test1.java
+++ test1.java
@@ -4,7 +4,7 @@ public class test1
     {
         String s;
         for (int i=0; i<5;i++) {
-            s = "premier test " + i ;
+            s = "Ligne n°" + i ;
             System.out.println(s);
         }
     }
